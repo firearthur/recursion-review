58b15b5d4681e9fc9e084d641693d9a612ace762
firearthur <thefirearthur@gmail.com>
2018-01-15 18:43:49 -0800

    Trying to come with algorithm for comma within key

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -9,32 +9,56 @@
 //   '{"foo": true, "bar": false, "baz": null}',
 //   '[1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999]',
 //   '{"boolean, true": true, "boolean, false": false, "null": null }',
-
-
+//    ['"boolean, true"','true','"boolean, false"','false,"null"','null']
+ 
 var parseJSON = function(json) {
   // your code goes here
   var firstChar = json[0];
 // debugger;
-  
+  //array handling
   if(firstChar === '['){
-    if (json === '[]') {
+    if (json === '[]'){
       return [];
     }
     var result = [];
     var delimitter = json.includes(', ') ? ', ' : ',';
     var pieceOfJson = json.slice(1, json.length-1).split(delimitter);  
-    for (var i = 0; i < pieceOfJson.length; i++) {
+    for (var i = 0; i < pieceOfJson.length; i++){
       result = result.concat(parseJSON(pieceOfJson[i]));
     }
     return result;
   }
+
+ // if(firstChar === '{'){
+ //    if (json === '{}'){
+ //      return {};
+ //    }
+    
+//split using ': '
+    // var key;
+    // var value;
+    // var result = {};
+    // var pieceOfJson = json.slice(1, json.length-1).split(': ');
+    // for (var i = 0; i < pieceOfJson.length; i++) {
+      
+    //   if (i % 2 === 0) {
+    //     key = parseJSON(pieceOfJson[i]);
+    //   } 
+    //   if (i % 2 === 1) {
+    //     value = parseJSON(pieceOfJson[i]);
+    //   }
+    //   result[key] = value;
+    // }
+
   // debugger;
+  // object handling
   if(firstChar === '{'){
     if (json === '{}') {
       return {};
     }
     var result = {};
-    var pieceOfJson = json.slice(1, json.length-1).split(', '); 
+    var pieceOfJson = json.slice(1, json.length-1).split(', '); //[1 ':', ':']  if indexOf(', ') < indexOf(': ') indeOf(', ', ':')
+    // var pieceOfJson = json.slice(1, json.length-1).split(': ');
     for (var i = 0; i < pieceOfJson.length; i++) {
       var keyValuePairs = pieceOfJson[i].split(': ');        
       result[parseJSON(keyValuePairs[0])] = parseJSON(keyValuePairs[1]);
@@ -45,19 +69,20 @@ var parseJSON = function(json) {
   if(firstChar === '"') {
     return json.slice(1, json.length-1);
   }
-
-  if(json === 'null'){
-    return null;
-  }
-  if(json === 'false'){
-    return false;
-  }
-  if(json === 'true'){
-    return true;
-  }
-  if(!isNaN(Number(json))){  //returns if it is a number including zero
-    return Number(json);
-  }
+  //  replaced with eval for the moment
+  // if(json === 'null'){
+  //   return null;
+  // }
+  // if(json === 'false'){
+  //   return false;
+  // }
+  // if(json === 'true'){
+  //   return true;
+  // }
+  // if(!isNaN(Number(json))){  //returns if it is a number including zero
+  //   return Number(json);
+  // }
+  return eval(json);
 };
 
 
